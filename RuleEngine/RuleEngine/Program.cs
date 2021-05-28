3232bdda6b9e44b8ed60d792d4385c7d92bf79b4
Sameer <Sameer@DESKTOP-SPET6TL>
2021-05-29 01:07:06 +0530

    Initial Commit for dummy Method

--- RuleEngine/RuleEngine/Program.cs
+++ RuleEngine/RuleEngine/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RuleEngine
 {
@@ -6,7 +7,34 @@ namespace RuleEngine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool repeat = true;
+
+            while (repeat)
+            {
+                List<Product> products = new List<Product>();
+
+                foreach (ProductEnum item in (ProductEnum[])Enum.GetValues(typeof(ProductEnum)))
+                {
+                    Console.WriteLine("How many products of type '" + item.ToString() + "'");
+                    string input = Console.ReadLine();
+
+                    products.AddRange(Product.getNumOfProducts(item, input));
+                }
+
+                PromotionService promotionService = new PromotionService();
+                decimal totalValue = promotionService.getTotalValue(products);
+                Console.WriteLine("Total Order Value : " + totalValue);
+
+                Console.WriteLine("Do you want to repeat the Transaction y/n");
+
+                string response = Console.ReadLine();
+
+                if(response == "N" || response == "n")
+                {
+                    repeat = false;
+                }
+            }
+            
         }
     }
 }
---
+++ RuleEngine/RuleEngine/PromotionService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RuleEngine
+{
+    public class PromotionService
+    {
+        public decimal getTotalValue(List<Product> products)
+        {
+            List<Promotion> list = PromotionList.getPromotionList();
+
+            Dictionary<ProductEnum, int> prodInfo = products.GroupBy(x => x.Id).ToDictionary(g => g.Key, g => g.ToList().Count());
+
+            decimal promoPrice = 0M;
+           
+            return promoPrice;
+        }
+    }
+}
