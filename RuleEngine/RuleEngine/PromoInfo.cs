ebbbcb9418c284cc29460395a661d35aaa3ff7b2
Sameer <Sameer@DESKTOP-SPET6TL>
2021-05-29 01:05:43 +0530

    PromoInfo Class Added

---
+++ RuleEngine/RuleEngine/PromoInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RuleEngine
+{
+    public class PromoInfo
+    {
+        public int promoCount { get; set; }
+        public int prodCount { get; set; }
+
+        public PromoInfo(int _promoCount, int _prodCount)
+        {
+            this.promoCount = _promoCount;
+            this.prodCount = _prodCount;
+        }
+    }
+}
