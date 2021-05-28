42ebac058093da2a5c68c421b6e00e7c9d6a1928
Sameer <Sameer@DESKTOP-SPET6TL>
2021-05-28 20:03:19 +0530

    Initial Project setup and Test Library added

---
+++ RuleEngine/RuleEngine/Promotion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RuleEngine
+{
+    public class Promotion
+    {
+        
+    }
+}
---
+++ RuleEngine/RuleEngineTest/RuleTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using RuleEngine;
+using System;
+
+namespace RuleEngineTest
+{
+    [TestFixture]
+    public class RuleTest
+    {
+        [Test]
+        public void testPromotionObject()
+        {
+            Promotion promotion = new Promotion();
+            Assert.IsNotNull(promotion);
+        }
+    }
+}
